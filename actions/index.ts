6396731fd24ccf9d0b3fc5666bcb321f7519b004
anishmrzn <anishmrzan@gmail.com>
2024-05-17 07:33:26 +0545

    useFormState

--- actions/index.ts
+++ actions/index.ts
@@ -16,3 +16,40 @@ export async function deleteSnippet(id: number) {
   });
   redirect("/");
 }
+
+export async function createSnippet(
+  formState: { message: string },
+  formData: FormData
+) {
+  try {
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+
+    //this needs tobe a server action
+    // "use server";
+    // return { messgae: "Title must be longer" };
+
+    if (typeof title !== "string" || title.length < 3) {
+      return { messgae: "Title must be longer" };
+    }
+    if (typeof code !== "string" || code.length < 3) {
+      return { messgae: "Title must be longer" };
+    }
+
+    await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return { message: err.message };
+    } else {
+      return {
+        messgae: "something went worng",
+      };
+    }
+  }
+  redirect("/");
+}
---
+++ app/snippets/new/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+interface ErrorPageProps {
+  error: Error;
+  reset: () => void;
+}
+export default function ErrorPage({ error }: ErrorPageProps) {
+  <div>{error.message}</div>;
+}
